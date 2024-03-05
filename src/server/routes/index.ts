4ed064e497225719ffcf929900903546a6ee6f11
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-03-04 23:52:54 -0300

    ✨ New feature: Finalizando métodos de CRUD de cidades, by Eduardo Xavier

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,9 +8,25 @@ router.get('/', (req, res) => {
 	return res.send('Olá, Dev!');
 });
 
+router.get('/cidades',
+	CidadesController.getAllValidation,
+	CidadesController.getAll
+);
+router.get('/cidades/:id',
+	CidadesController.getByIdValidation,
+	CidadesController.getById
+);
 router.post('/cidades',
 	CidadesController.createValidation,
 	CidadesController.create
+);
+router.put('/cidades/:id',
+	CidadesController.updateByIdValidation,
+	CidadesController.updateById
+);
+router.delete('/cidades/:id',
+	CidadesController.deleteByIdValidation,
+	CidadesController.deleteById
 );
 
 export { router };
